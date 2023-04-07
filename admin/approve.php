1fb8c34a3fb6d5e37506f29c988f41810fb566b9
JRendel <mrskybrine@gmail.com>
2023-04-07 21:06:30 +0800

    added account verification to admin

---
+++ admin/approve.php
@@ -0,0 +1,12 @@
+<?php
+include('../connect.php');
+// get the auth_id
+$auth_id = $_GET['auth_id'];
+
+// update the acc_status to 2
+$sql = "UPDATE tbl_auth SET acc_status = 1 WHERE auth_id = '$auth_id'";
+mysqli_query($database, $sql);
+
+// redirect to tutor_approval.php
+header('location: tutor_approval.php');
+?>
